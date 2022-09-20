7647d88712cbc26f5e31c4ac4c25b7aba142f3c8
Shigma <1700011071@pku.edu.cn>
2022-09-20 23:41:57 +0800

    feat: add more syntax

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export const Config: Schema<Config> = Schema.object({})
 const corpus: Dict<string[]> = require('./corpus')
 
 const keys = Object.keys(corpus)
-keys.push('p')
+keys.push('m')
 
 const regexp = new RegExp(`%(${keys.join('|')})`, 'g')
 
@@ -18,22 +18,25 @@ const desc = {
   vi: '不及物动词',
   adj: '形容词',
   adv: '副词',
+  pl: '地点',
   n: '名词',
-  p: '群成员',
-  t: '时间',
+  m: '群成员',
+  tf: '时间点',
+  tp: '时间段',
 }
 
 export function apply(ctx: Context) {
-  ctx.command('nonsense <text:rawtext>')
+  ctx.command('nonsense <text:rawtext>', '输出随机句子')
     .alias('ns')
     .action(async ({ session }, text) => {
       if (!text) {
         return Object.entries(desc).map(([k, v]) => `${k}: ${v}`).join('\n')
       }
-      const members = text.includes('%p') ? await session.bot.getGuildMemberList(session.guildId) : []
+      const members = text.includes('%m') ? await session.bot.getGuildMemberList(session.guildId) : []
       return text.replace(regexp, (_, key) => {
-        if (key === 'p') {
-          return Random.pick(members).nickname
+        if (key === 'm') {
+          const member = Random.pick(members)
+          return member.nickname || member.username
         } else {
           return Random.pick(corpus[key])
         }
